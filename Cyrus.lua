cec9496e0ceb1d31af5554c591ceaccbe2476209
xx0m <63579245+xx0m@users.noreply.github.com>
2020-04-20 11:28:42 +1000

    Update Cyrus.lua
    Removed old testing code

--- Cyrus.lua
+++ Cyrus.lua
@@ -1084,20 +1084,6 @@ C['Votes'] = {
     ['VoteOptions'] = {}
 }
 
-local char_to_hex = function(c)
-    return string.format("%%%02X", string.byte(c))
-end
-
-local function urlencode(url)
-    if url == nil then
-        return
-    end
-    url = url:gsub("\n", "\r\n")
-    url = url:gsub("([^%w ])", char_to_hex)
-    url = url:gsub(" ", "+")
-    return url
-end
-
 C['Funcs'] = {
     ['PrintChangelogs'] = function()
         local col = C['Colours']['RGB']
